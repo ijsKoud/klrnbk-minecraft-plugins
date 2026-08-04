d6798fa7d22c892bdf7a4061417b5b3669430d67
Daan <daan@klrnbk.nl>
2026-08-04 23:55:59 +0200

    feat: add database tables and models

---
+++ src/main/kotlin/net/terraimperia/discord/rewards/services/database/tables/DiscordLinkRequestCode.kt
@@ -0,0 +1,13 @@
+package net.terraimperia.discord.rewards.services.database.tables
+
+import org.jetbrains.exposed.dao.id.UUIDTable
+import java.util.UUID
+
+object DiscordLinkRequestCodeTable : UUIDTable("discord_link_request_code") {
+    val requestCode = varchar("request_code", 255)
+}
+
+data class DiscordLinkRequestCode(
+    val uuid: UUID,
+    val code: String,
+)
