f0b0b3cfd1e1c5f4bc60c1af078933dca2ea2184
Daan <daan@klrnbk.nl>
2026-08-07 15:14:56 +0200

    feat: add service feature to create registration

--- src/main/kotlin/net/terraimperia/discord/rewards/services/link/LinkService.kt
+++ src/main/kotlin/net/terraimperia/discord/rewards/services/link/LinkService.kt
@@ -5,8 +5,11 @@ import com.google.inject.Singleton
 import kotlinx.datetime.LocalDateTime
 import kotlinx.datetime.format
 import net.terraimperia.discord.rewards.services.database.repository.DiscordLinkRepository
+import net.terraimperia.discord.rewards.services.database.repository.DiscordLinkRequestCodeRepository
 import net.terraimperia.discord.rewards.services.database.repository.PlayerRegistryDetailsRepository
+import net.terraimperia.discord.rewards.services.database.tables.DiscordLinkRequestCode
 import net.terraimperia.discord.rewards.services.link.models.LinkStatusModel
+import net.terraimperia.discord.rewards.utils.getCryptographicRandomText
 import java.util.UUID
 
 @Singleton
@@ -15,6 +18,7 @@ class LinkService
     constructor(
         private val playerRegistryDetailsRepository: PlayerRegistryDetailsRepository,
         private val discordLinkRepository: DiscordLinkRepository,
+        private val discordLinkRequestCodeRepository: DiscordLinkRequestCodeRepository,
     ) {
         fun getStatus(id: UUID): LinkStatusModel {
             val details = playerRegistryDetailsRepository.findById(id) ?: return LinkStatusModel(isLinked = false)
@@ -25,5 +29,21 @@ class LinkService
                 lastLinkDate = details.lastRegistryDate.format(LocalDateTime.Formats.ISO),
                 discordId = discordLink.discordId.toString(),
             )
+        }
+
+        fun getExistingRegistrationCode(id: UUID): String? {
+            val details = discordLinkRequestCodeRepository.findById(id) ?: return null
+            return details.code
+        }
+
+        fun createRegistrationCode(id: UUID): String {
+            val existingCode = getExistingRegistrationCode(id)
+            if (existingCode != null) return existingCode
+
+            val code = getCryptographicRandomText(16)
+            val details = DiscordLinkRequestCode(id, code)
+
+            discordLinkRequestCodeRepository.create(details)
+            return code
         }
     }
