44b528b583f825d41c7fcf2df94649cf26c48239
Daan <daan@klrnbk.nl>
2026-08-07 15:57:22 +0200

    feat: add /link unlink command

--- src/main/kotlin/net/terraimperia/discord/rewards/facade/LinkFacade.kt
+++ src/main/kotlin/net/terraimperia/discord/rewards/facade/LinkFacade.kt
@@ -9,6 +9,8 @@ import net.kyori.adventure.text.minimessage.tag.resolver.Placeholder
 import net.terraimperia.discord.rewards.services.config.ConfigService
 import net.terraimperia.discord.rewards.services.link.LinkService
 import net.terraimperia.discord.rewards.services.minimessage.MiniMessageService
+import net.terraimperia.discord.rewards.utils.formatInstantToDateTimeString
+import kotlin.time.Clock
 
 @Singleton
 class LinkFacade
@@ -18,16 +20,21 @@ class LinkFacade
         private val miniMessageService: MiniMessageService,
         private val linkService: LinkService,
     ) {
+        fun hasPermission(source: CommandSource): Boolean {
+            if (source !is Player) {
+                val message = miniMessageService.deserialize(configService.config.messages.playerOnlyCommand)
+                source.sendMessage(message)
+
+                return false
+            }
+
+            return true
+        }
+
         fun status(context: CommandContext<CommandSource>): Int {
             val yesString = configService.config.messages.yes
             val noString = configService.config.messages.no
             val undefinedString = configService.config.messages.undefined
-
-            if (context.source !is Player) {
-                val message = miniMessageService.deserialize(configService.config.messages.playerOnlyCommand)
-                context.source.sendMessage(message)
-                return 0
-            }
 
             val id = (context.source as Player).uniqueId
             val linkDetails = linkService.getStatus(id)
@@ -44,12 +51,6 @@ class LinkFacade
         }
 
         fun register(context: CommandContext<CommandSource>): Int {
-            if (context.source !is Player) {
-                val message = miniMessageService.deserialize(configService.config.messages.playerOnlyCommand)
-                context.source.sendMessage(message)
-                return 0
-            }
-
             val player = context.source as Player
             val id = player.uniqueId
             val linkDetails = linkService.getStatus(id)
@@ -66,6 +67,34 @@ class LinkFacade
                     configService.config.messages.registrationCode,
                     Placeholder.parsed("link-code", linkCode),
                 )
+            context.source.sendMessage(message)
+            return 1
+        }
+
+        fun unlink(context: CommandContext<CommandSource>): Int {
+            val player = context.source as Player
+            val id = player.uniqueId
+            val linkDetails = linkService.getStatus(id)
+
+            if (!linkDetails.isLinked) {
+                val message = miniMessageService.deserialize(configService.config.messages.notLinked)
+                context.source.sendMessage(message)
+                return 0
+            }
+
+            val playersNextUnlinkDate = linkService.getPlayersNextUnlinkDate(id, configService.config.registration.resetLimit)
+            if (playersNextUnlinkDate != null && playersNextUnlinkDate > Clock.System.now()) {
+                val message =
+                    miniMessageService.deserialize(
+                        configService.config.messages.unlinkTooSoon,
+                        Placeholder.unparsed("date", formatInstantToDateTimeString(playersNextUnlinkDate)),
+                    )
+                context.source.sendMessage(message)
+                return 0
+            }
+
+            linkService.unlink(id)
+            val message = miniMessageService.deserialize(configService.config.messages.unlinked)
             context.source.sendMessage(message)
             return 1
         }
--- src/main/kotlin/net/terraimperia/discord/rewards/services/link/LinkService.kt
+++ src/main/kotlin/net/terraimperia/discord/rewards/services/link/LinkService.kt
@@ -2,15 +2,17 @@ package net.terraimperia.discord.rewards.services.link
 
 import com.google.inject.Inject
 import com.google.inject.Singleton
-import kotlinx.datetime.LocalDateTime
-import kotlinx.datetime.format
+import kotlinx.datetime.DateTimeUnit
+import kotlinx.datetime.plus
 import net.terraimperia.discord.rewards.services.database.repository.DiscordLinkRepository
 import net.terraimperia.discord.rewards.services.database.repository.DiscordLinkRequestCodeRepository
 import net.terraimperia.discord.rewards.services.database.repository.PlayerRegistryDetailsRepository
 import net.terraimperia.discord.rewards.services.database.tables.DiscordLinkRequestCode
 import net.terraimperia.discord.rewards.services.link.models.LinkStatusModel
+import net.terraimperia.discord.rewards.utils.formatInstantToDateTimeString
 import net.terraimperia.discord.rewards.utils.getCryptographicRandomText
 import java.util.UUID
+import kotlin.time.Instant
 
 @Singleton
 class LinkService
@@ -26,7 +28,7 @@ class LinkService
 
             return LinkStatusModel(
                 isLinked = details.isRegistered,
-                lastLinkDate = details.lastRegistryDate.format(LocalDateTime.Formats.ISO),
+                lastLinkDate = formatInstantToDateTimeString(details.lastRegistryDate),
                 discordId = discordLink.discordId.toString(),
             )
         }
@@ -45,5 +47,18 @@ class LinkService
 
             discordLinkRequestCodeRepository.create(details)
             return code
+        }
+
+        fun getPlayersNextUnlinkDate(
+            id: UUID,
+            waitDuration: Int,
+        ): Instant? {
+            val details = playerRegistryDetailsRepository.findById(id) ?: return null
+            return details.lastRegistryDate.plus(waitDuration, DateTimeUnit.SECOND)
+        }
+
+        fun unlink(id: UUID) {
+            discordLinkRepository.deleteById(id)
+            playerRegistryDetailsRepository.resetLinkStatus(id)
         }
     }
