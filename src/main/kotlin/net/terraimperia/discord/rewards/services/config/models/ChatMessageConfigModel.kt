a0229fa83e3fbf9a4f36d606ceb479ac11f04fa1
Daan <daan@klrnbk.nl>
2026-08-07 15:56:07 +0200

    feat: add more chat messages

--- src/main/kotlin/net/terraimperia/discord/rewards/services/config/models/ChatMessageConfigModel.kt
+++ src/main/kotlin/net/terraimperia/discord/rewards/services/config/models/ChatMessageConfigModel.kt
@@ -21,7 +21,7 @@ class ChatMessageConfigModel {
     val alreadyLinked: String =
         arrayOf(
             "<red>You are already linked to a Discord account!",
-            "You can unlink your account by using the <click:suggest_command:'/unlink'><bold>/unlink</bold></click> command.</red>",
+            "You can unlink your account by using the <click:suggest_command:'/link unlink'><bold>/link unlink</bold></click> command.</red>",
         ).joinToString("\n")
 
     @Setting("registration-code")
@@ -30,6 +30,27 @@ class ChatMessageConfigModel {
             "<green>You can register your account using the following code: <link-code>",
             "Use the following command (click to copy) in our Discord server to complete the process.\n",
             "<click:coppy_to_clipboard:'/link <link-code>'><bold>/link <link-code></bold></click></green>",
+        ).joinToString("\n")
+
+    @Setting("discord-not-linked")
+    val notLinked: String =
+        arrayOf(
+            "<red>You have not linked to any Discord account yet!",
+            " Start by linking using the <click:suggest_command:'/link'><bold>/unlink</bold></click> command.</red>",
+        ).joinToString("\n")
+
+    @Setting("unlink-too-soon")
+    val unlinkTooSoon: String =
+        arrayOf(
+            "<red>You cannot unlink your account yet!",
+            "Please wait until <white><date></white> before trying again.</red>",
+        ).joinToString("\n")
+
+    @Setting("unlinked")
+    val unlinked: String =
+        arrayOf(
+            "<green>Your account has been successfully unlinked from Discord!",
+            "Want to link it again? Use the <click:suggest_command:'/link'><bold>/link</bold></click> command</green>",
         ).joinToString("\n")
 
     val yes: String = "<yes>Yes</yes>"
