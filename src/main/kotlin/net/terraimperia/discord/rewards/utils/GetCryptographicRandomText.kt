df198294d2ca5cb9cf5e9a169d336d896214c9e1
Daan <daan@klrnbk.nl>
2026-08-07 15:14:36 +0200

    feat: add generator for cryptographic random text

---
+++ src/main/kotlin/net/terraimperia/discord/rewards/utils/GetCryptographicRandomText.kt
@@ -0,0 +1,18 @@
+package net.terraimperia.discord.rewards.utils
+
+import java.security.SecureRandom
+
+object RegistrationCodeGenerator {
+    private val random = SecureRandom()
+
+    private const val CHARSET = "ABCDEFGHJKLMNPQRSTUVWXYZ23456789"
+
+    fun generate(length: Int = 8): String =
+        buildString(length) {
+            repeat(length) {
+                append(CHARSET[random.nextInt(CHARSET.length)])
+            }
+        }
+}
+
+fun getCryptographicRandomText(length: Int): String = RegistrationCodeGenerator.generate(length)
